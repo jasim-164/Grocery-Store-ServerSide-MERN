d60efc18030de6f49fc1afcc7a244aa9c5acc5a0
jasim-164 <jasimuddincse1703164@gmail.com>
2022-05-09 01:45:59 +0600

    heroku error 1

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r19pq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
 const client = new MongoClient(uri, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
