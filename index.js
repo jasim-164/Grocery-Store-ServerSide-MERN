4fbb08d6c47c31cdd1e5f37936f1482a3a346de6
jasim-164 <jasimuddincse1703164@gmail.com>
2022-05-09 20:26:18 +0600

    JWT access token create & order collection add

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
-require('dotenv').config()
+require('dotenv').config();
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(403).send({ message: 'Forbidden access' });
+      }
+      console.log('decoded', decoded);
+      req.decoded = decoded;
+      next();
+  })
+}
+
+
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r19pq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -16,12 +36,25 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1 });
 
-
+//require('crypto').randomBytes(64).toString('hex')
 async function run() {
+
   try {
     await client.connect();
     const database = client.db('grocerystock');
     const productCollection = database.collection('products');
+    const orderCollection = database.collection('order');
+        
+        //FOR AUTH JWT TOKEN
+        app.post('/login',async(req,res) => {
+          const user =req.body;
+          const accessToken=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:'1d'
+          });
+          console.log({accessToken});
+          res.send({accessToken});
+
+        })
         // get api to read all products
     //http://localhost:4000/products
     app.get("/products", async (req, res) => {
@@ -73,6 +106,28 @@ async function run() {
     // console.log('from put method',id)
     res.send(result);
      } )
+
+     
+  // Order Collection API
+
+            app.get('/order', verifyJWT, async (req, res) => {
+              const decodedEmail = req.decoded.email;
+              const email = req.query.email;
+              if (email === decodedEmail) {
+                  const query = { email: email };
+                  const cursor = orderCollection.find(query);
+                  const orders = await cursor.toArray();
+                  res.send(orders);
+              }
+              else{
+                  res.status(403).send({message: 'forbidden access'})
+              }
+          })
+          app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
 
 
 
